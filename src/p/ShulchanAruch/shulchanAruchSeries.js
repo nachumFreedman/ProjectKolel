3fae36f6bb871af62f4eda4495bff4567299ae6c
nik frank <nikbfrank@gmail.com>
2017-08-31 19:41:59 +0300

    shulchan-aruch starting

--- src/p/ShulchanAruch/shulchanAruchSeries.js
+++ src/p/ShulchanAruch/shulchanAruchSeries.js
@@ -1,52 +1,52 @@
 export default [
   {
-    title: "Zera'im",
+    title: 'MB vol. 1',
+    id: 0,
+    books: [
+      { id: 0, title: '1-7 \n Arising' },
+      { id: 1, title: '8-24 \n Tzitzis' },
+      { id: 2, title: '25-45 \n Tefilin' },
+      { id: 3, title: '46-57 \n Morning Prayers' },
+      { id: 4, title: '58-102 \n Keri\'as Shema' },
+      { id: 5, title: '103-127 \n Shemoneh Esreh' },
+    ],
+  }, {
+    title: 'MB vol. 2',
     id: 1,
     books: [
-      {
-        title: 'Berachos',
-        id: 1,
-      },
-      {
-        title: "Pe'ah",
-        id: 2,
-      },
-      {
-        title: 'Demai',
-        id: 3,
-      },
-      {
-        title: "Kil'ayim",
-        id: 4,
-      },
-      {
-        title: "Shevi'is",
-        id: 5,
-      },
-      {
-        title: 'Terumos',
-        id: 6,
-      },
-      {
-        title: "Ma'asros",
-        id: 7,
-      },
-      {
-        title: "Ma'aser Sheni",
-        id: 8,
-      },
-      {
-        title: 'Chalah',
-        id: 9,
-      },
-      {
-        title: 'Oriah',
-        id: 10,
-      },
-      {
-        title: 'Bikurim',
-        id: 11,
-      }
+      { id: 0, title: '128-130 \n Birkat Kohanim' },
+      { id: 1, title: '131-134 \n End of Prayers' },
+      { id: 2, title: '135-149 \n Reading the Torah' },
+      { id: 3, title: '150-156 \n The Synagogue' },
+      { id: 4, title: '157-165 \n Washing for Bread' },
+      { id: 5, title: '166-168 \n Eating Bread' },
+    ]
+  }, {
+    title: 'MB vol. 2',
+    id: 6,
+    books: [
+      { id: 6, title: '169-180 \n Laws of Meals' },
+      { id: 7, title: '181-201 \n Blessing After Meals' },
+      { id: 8, title: '202-215 \n Blessing for Bread' },
+      { id: 9, title: '216-231 \n Assorted Blessings' },
+      { id: 10, title: '232-234 \n Minchah Prayers' },
+      { id: 11, title: '235-241 \n Ma\'ariv and Night' },
     ],
+  }, {
+    title: 'MB vol. 3',
+    id: 2,
+    books: [],
+  }, {
+    title: 'MB vol. 4',
+    id: 3,
+    books: [],
+  }, {
+    title: 'MB vol. 5',
+    id: 4,
+    books: [],
+  }, {
+    title: 'MB vol. 6',
+    id: 5,
+    books: [],
   },
 ];
