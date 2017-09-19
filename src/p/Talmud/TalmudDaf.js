c7f97b6d6e0c245ecb95f2ea581b43139cc47176
nik frank <nikbfrank@gmail.com>
2017-09-19 13:16:06 +0300

    read daf from server

--- src/p/Talmud/TalmudDaf.js
+++ src/p/Talmud/TalmudDaf.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
+import ReactMarkdown from 'react-markdown';
+
 import sampleDaf from '../../network/GetHtmlDaf';
 
 class TalmudDaf extends Component {
+  state = {
+    md: ''
+  }
+  
+  componentDidMount(){
+    fetch('https://daf-stream.herokuapp.com/dafs/raw/kesuvos/insites/ks-dt-'+
+          ('000' + window.location.hash.slice(window.location.hash.lastIndexOf('/')+1 )).substr(-3)
+         +'.htm')
+      .then(pon => pon.text())
+      .then(pon => this.setState({ md: pon }) )
+  }
+  
   render(){
     return (
-      <div dangerouslySetInnerHTML={sampleDaf}/>
+      <ReactMarkdown source={this.state.md}/>
     );
   }
 }
-
+//       <div dangerouslySetInnerHTML={sampleDaf}/>
 export default TalmudDaf;
