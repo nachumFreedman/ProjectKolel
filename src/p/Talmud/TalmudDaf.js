e4a2e40a28d34f3c1838105841e2ba119df28604
nachumfreedman <nochum09@gmail.com>
2017-10-11 12:53:45 +0300

    calling new api for daff data

--- src/p/Talmud/TalmudDaf.js
+++ src/p/Talmud/TalmudDaf.js
@@ -7,17 +7,23 @@ import sampleDaf from '../../network/GetHtmlDaf';
 const domain = 'https://daf-stream.herokuapp.com';
 //const domain = 'http://localhost:3000';
 
+const urlFromPathName = (pathname) => {  
+  const pathParams = pathname.split('/');
+  const url = domain + "/" + 'dafs/raw/' + pathParams[2] + "/" + pathParams[3] + "/" + pathParams[4];
+  return url;
+} 
+
 class TalmudDaf extends Component {
   state = {
     md: ''
   }
   
   componentDidMount(){
-    fetch(domain+'/dafs/raw/kesuvos/insites/ks-dt-'+
-          ('000' + window.location.hash.slice(window.location.hash.lastIndexOf('/')+1 )).substr(-3)
-         +'.htm')
-      .then(pon => pon.text())
-      .then(pon => this.setState({ md: pon }) )
+    console.log(this.props)
+    console.log(urlFromPathName(this.props.location.pathname))
+    fetch(urlFromPathName(this.props.location.pathname))
+           .then(pon => pon.text())
+           .then(pon => this.setState({ md: pon }) )
   }
   
   render(){
@@ -28,3 +34,9 @@ class TalmudDaf extends Component {
 }
 //       <div dangerouslySetInnerHTML={sampleDaf}/>
 export default TalmudDaf;
+
+/*
+   domain+'/dafs/raw/kesuvos/insites/ks-dt-'+
+   ('000' + window.location.hash.slice(window.location.hash.lastIndexOf('/')+1 )).substr(-3)
+   +'.htm'
+ */
